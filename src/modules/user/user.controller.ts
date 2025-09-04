356b977533ebbb88bfa03f4b89957fc360041119
FatmaMoataz <fatmamoataz65@gmail.com>
2025-09-04 13:17:07 +0200

    validation & file size limit

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -4,12 +4,12 @@ import { authentication } from "../../middleware/authentication.middleware";
 import { validation } from "../../middleware/validation.middleware";
 import * as validators from './user.validation'
 import { TokenEnum } from "../utils/security/token.security";
-import { cloudFileUpload, StorageEnum } from "../utils/multer/cloud.multer";
+import { cloudFileUpload, fileValidation, StorageEnum } from "../utils/multer/cloud.multer";
 
 const router = Router()
 
 router.get("/", authentication(),userService.profile)
-router.patch("/profile-img", authentication(), cloudFileUpload({storageApproach:StorageEnum.disk}).single("img"),userService.profileImg)
+router.patch("/profile-img", authentication(), cloudFileUpload({validation: fileValidation.img ,storageApproach:StorageEnum.disk}).single("img"),userService.profileImg)
 router.post("/refresh-token", authentication(TokenEnum.refresh), userService.refreshToken)
 router.post("/logout", authentication(), validation(validators.logout),userService.logout)
 export default router
--- src/modules/utils/multer/cloud.multer.ts
+++ src/modules/utils/multer/cloud.multer.ts
@@ -1,11 +1,32 @@
-import multer from "multer";
+import multer, { FileFilterCallback } from "multer";
+import os from 'node:os'
+import {v4 as uuid} from 'uuid'
+import { BadRequest } from "../response/error.response";
+import type{ Request } from "express";
 
 export enum StorageEnum {
 memory = "memory",
 disk = "disk"
 }
 
-export const cloudFileUpload = ({storageApproach=StorageEnum.memory}:{storageApproach?:StorageEnum}):multer.Multer => {
-    const storage = storageApproach === StorageEnum.memory ?  multer.memoryStorage() : multer.diskStorage({})
-    return multer({storage})
+export const fileValidation = {
+img:["image/jpeg", "image/png", "image/gif"]
+}
+
+export const cloudFileUpload = ({validation=[],storageApproach=StorageEnum.memory, maxSizeMB=2}:{validation?:string[],storageApproach?:StorageEnum, maxSizeMB?:number}):multer.Multer => {
+    const storage = storageApproach === StorageEnum.memory ?  multer.memoryStorage() : multer.diskStorage({
+        destination:os.tmpdir(),
+        filename: function(req:Request, file:Express.Multer.File, callback) {
+callback(null, `${uuid()}_${file.originalname}`)
+        }
+    })
+    function fileFilter(req:Request, file:Express.Multer.File, callback:FileFilterCallback) {
+if(!validation.includes(file.mimetype)) {
+return callback(new BadRequest("Validation error ❌", {validationErrors:[{key:"file",
+    issues:[{path:"file", message:"Invalid file format"}]
+}]}))
+}
+return callback(null, true)
+    }
+    return multer({fileFilter, limits:{fileSize:maxSizeMB * 1024 * 1024} ,storage})
 }
