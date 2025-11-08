ff97cca27543aec98b55524cb09c4a6e22da9dcc
FatmaMoataz <fatmamoataz65@gmail.com>
2025-11-08 13:47:24 +0200

    middleware - handshake - error handling

--- FE/index.js
+++ FE/index.js
@@ -1,8 +1,14 @@
-const clientIo = io("http://localhost:3000/")
+const clientIo = io("http://localhost:3000/" , {
+    auth: {authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OGI5NjdjMjAxMDIwNTViOTkwZGY5MGUiLCJqdGkiOiIyODA4YjU5ZC1iYzQ1LTRhNGUtODNhNS1mZTlhODdkZjA2NWMiLCJpYXQiOjE3NTk5MDkxNDQsImV4cCI6MTc1OTkxMjc0NH0.v0xMY5wEyOGMGOr1n5n2U-_WIaFsLCP9KMrJ_kZo31Y"}
+})
 const clientIo2 = io("http://localhost:3000/admin")
 
 clientIo.on("connect", () => {
     console.log("Server establish connection successfully");
+})
+
+clientIo.on("connect_error", (error) => {
+    console.log(`Connection error ${error.message}`);
 })
 
 clientIo.emit("sayHi" , "Hello from FE to BE" , (res) => {
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,115 +1,172 @@
-import {resolve} from 'node:path'
-import {config} from 'dotenv'
-config({path: resolve("./config/.env.development")})
+import { resolve } from "node:path";
+import { config } from "dotenv";
+config({ path: resolve("./config/.env.development") });
 
-import type {Express,  Request,  Response} from "express"
-import express from "express"
+import type { Express, Request, Response } from "express";
+import express from "express";
 
-import cors from "cors"
-import helmet from "helmet"
-import {rateLimit} from 'express-rate-limit'
+import cors from "cors";
+import helmet from "helmet";
+import { rateLimit } from "express-rate-limit";
 
-import {authRouter, userRouter, postRouter} from './modules'
+import { authRouter, userRouter, postRouter } from "./modules";
 
-import { BadRequest, globalErrorHandling } from './modules/utils/response/error.response'
+import {
+  BadRequest,
+  globalErrorHandling,
+} from "./modules/utils/response/error.response";
 
-import connectDB from './DB/connection.db.js'
-import { createGetPreSignedLink, getFile} from './modules/utils/multer/s3.config'
+import connectDB from "./DB/connection.db.js";
+import {
+  createGetPreSignedLink,
+  getFile,
+} from "./modules/utils/multer/s3.config";
 
-import {promisify} from 'node:util'
-import { pipeline } from 'node:stream'
+import { promisify } from "node:util";
+import { pipeline } from "node:stream";
 
-const createS3WriteStreamPipe = promisify(pipeline)
+const createS3WriteStreamPipe = promisify(pipeline);
 
 const limiter = rateLimit({
-    windowMs:60*6000,
-    limit:2000,
-    message:{error:"Too many request please try again later"}
-})
+  windowMs: 60 * 6000,
+  limit: 2000,
+  message: { error: "Too many request please try again later" },
+});
 
-import {Server, Socket} from 'socket.io'
+import { Server, Socket } from "socket.io";
+import { decodeToken, TokenEnum } from "./modules/utils/security/token.security";
 
-const bootstrap = async():Promise<void> => {
-const app:Express = express()
-const port:number | string = process.env.PORT || 5000
-app.use(cors(),express.json(),helmet())
+const connectedSockets: string[] = [];
+const bootstrap = async (): Promise<void> => {
+  const app: Express = express();
+  const port: number | string = process.env.PORT || 5000;
+  app.use(cors(), express.json(), helmet());
 
-app.use(limiter)
-await connectDB()
+  app.use(limiter);
+  await connectDB();
 
-// app-routing
-app.get('/',(req:Request, res:Response) => {
-    res.json({message:`Welcome to ${process.env.APPLICATION_NAME} backend landing page`})
-})
-// modules
-app.use("/auth", authRouter)
-app.use("/user", userRouter)
-app.use("/post", postRouter)
+  // app-routing
+  app.get("/", (req: Request, res: Response) => {
+    res.json({
+      message: `Welcome to ${process.env.APPLICATION_NAME} backend landing page`,
+    });
+  });
+  // modules
+  app.use("/auth", authRouter);
+  app.use("/user", userRouter);
+  app.use("/post", postRouter);
 
-app.use(globalErrorHandling)
+  app.use(globalErrorHandling);
 
-app.get("/uploads/*path", async(req:Request, res:Response):Promise<void> => {
-    const {downloadName, download="false"} = req.query as {downloadName?:string, download?:string} 
-    const {path} = req.params as unknown as {path:string[]}
-    const Key = path.join("/")
-    const s3Response = await getFile({Key})
-    if(!s3Response?.Body) {
-throw new BadRequest("Failed to fetch this asset")
+  app.get(
+    "/uploads/*path",
+    async (req: Request, res: Response): Promise<void> => {
+      const { downloadName, download = "false" } = req.query as {
+        downloadName?: string;
+        download?: string;
+      };
+      const { path } = req.params as unknown as { path: string[] };
+      const Key = path.join("/");
+      const s3Response = await getFile({ Key });
+      if (!s3Response?.Body) {
+        throw new BadRequest("Failed to fetch this asset");
+      }
+      res.set("Cross-Origin-Resource-Policy", "cross-origin");
+      res.setHeader(
+        "Content-type",
+        `${s3Response.ContentType || "application/octet-stream"}`
+      );
+      if (download === "true") {
+        res.setHeader(
+          "Content-Disposition",
+          `attachments: filename="${downloadName || Key.split("/").pop()}"`
+        );
+      }
+      return await createS3WriteStreamPipe(
+        s3Response.Body as NodeJS.ReadableStream,
+        res
+      );
     }
-    res.set("Cross-Origin-Resource-Policy" , "cross-origin")
-    res.setHeader("Content-type", `${s3Response.ContentType || "application/octet-stream"}`)
-    if(download === "true") {
-    res.setHeader("Content-Disposition", `attachments: filename="${downloadName || Key.split("/").pop()}"`)
+  );
+
+  app.get(
+    "/uploads/pre-signed/*path",
+    async (req: Request, res: Response): Promise<Response> => {
+      const {
+        downloadName,
+        download = "false",
+        expiresIn = 120,
+      } = req.query as {
+        downloadName?: string;
+        download?: string;
+        expiresIn?: number;
+      };
+      const { path } = req.params as unknown as { path: string[] };
+      const Key = path.join("/");
+      const url = await createGetPreSignedLink({
+        Key,
+        download,
+        downloadName: downloadName as string,
+        expiresIn,
+      });
+      return res.json({ message: "Done", data: { url } });
     }
-return await createS3WriteStreamPipe(s3Response.Body as NodeJS.ReadableStream, res)
-})
+  );
 
-app.get("/uploads/pre-signed/*path", async(req:Request, res:Response):Promise<Response> => {
-    const {downloadName, download="false", expiresIn=120} = req.query as {downloadName?:string, download?:string, expiresIn?:number} 
-    const {path} = req.params as unknown as {path:string[]}
-    const Key = path.join("/")
-    const url = await createGetPreSignedLink({Key, download, downloadName: downloadName as string, expiresIn})
-return res.json({message:"Done", data:{url}})
-})
+  // invalid route
+  app.use("{/*dummy}", (req: Request, res: Response) => {
+    return res.status(404).json({ message: "Invalid routing" });
+  });
 
-// invalid route
-app.use("{/*dummy}",(req:Request, res:Response) => {return res.status(404).json({message:'Invalid routing'})})
+  const httpServer = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 
-const httpServer = app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);  
-})
-
-const io = new Server(httpServer  , {
+  const io = new Server(httpServer, {
     cors: {
-        origin:"*",
-    }
-})
-// http://localhost:3000/
-io.on("connection", (socket: Socket) => {
-console.log(socket);
-socket.on("sayHi" , (data , callback) => {
-console.log(data);
-callback("Hello BE To FE")
-})
-socket.emit("productStock" , {productId:"d3h4tag" , quantity:5} , (res:string) => {
-console.log({res});
-
-})
-socket.on("disconnect" , () => {
-    console.log(`logout from ${socket.id}`);
-    
-})
-})
-
-// http://localhost:3000/admin
-// io.of("/admin").on("connection", (socket: Socket) => {
-// console.log(`Admin`, socket.id);
-// socket.on("disconnect" , () => {
-//     console.log(`logout from ${socket.id}`);
-    
-// })
-// })
-
+      origin: "*",
+    },
+  });
+  io.use(async(socket:Socket , next) => {
+try {
+    const {user , decoded} = await decodeToken({
+        authorization: socket.handshake?.auth.authorization || '', 
+        tokenType:TokenEnum.access
+    })
+    //  new BadRequest("Failed in authentication middleware")
+     next()
+} catch (error:any) {
+    next(error)
 }
+  })
+  // http://localhost:3000/
+  io.on("connection", (socket: Socket) => {
+    console.log(socket);
+    connectedSockets.push(socket.id);
+    // socket.on("sayHi" , (data , callback) => {
+    // console.log(data);
+    // callback("Hello BE To FE")
+    // })
+    socket.emit(
+      "productStock",
+      { productId: "d3h4tag", quantity: 5 },
+    //   (res: string) => {
+    //     console.log({ res });
+    //   }
+    );
+    socket.on("disconnect", () => {
+      console.log(`logout from ${socket.id}`);
+    });
+  });
 
-export default bootstrap
+  // http://localhost:3000/admin
+  // io.of("/admin").on("connection", (socket: Socket) => {
+  // console.log(`Admin`, socket.id);
+  // socket.on("disconnect" , () => {
+  //     console.log(`logout from ${socket.id}`);
+
+  // })
+  // })
+};
+
+export default bootstrap;
